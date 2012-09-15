a48371af3725c218f7dca4ee46d0551fb7269567
Ruogu Hu <roc@broctany.com>
2012-09-15 18:27:18 -0400

    TFScoreTest of clustering

--- Feedlosophor/src/feedlosophor/scoring/TFScoreTest.java
+++ Feedlosophor/src/feedlosophor/scoring/TFScoreTest.java
@@ -1,13 +1,19 @@
 package feedlosophor.scoring;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+
+import org.json.JSONArray;
+
+import feedlosophor.clusterer.HClusterer;
 
 public class TFScoreTest {
 
 	public static void main(String[] args) {
 		Test();
+	        TestCluster();
 	}
 
 	public static void Test() {
@@ -74,4 +80,57 @@ public class TFScoreTest {
 		}
 		return sb.toString();
 	}
+	
+	       public static void TestCluster() {
+
+
+	                String[] input = {readFile("test_files/1_romney.txt"),
+	                                readFile("test_files/2_romney.txt"),
+	                                readFile("test_files/3_romney.txt"),
+	                                readFile("test_files/4_romney.txt"),
+	                                readFile("test_files/5_romney.txt"),
+	                                readFile("test_files/6_romney.txt"),
+	                                readFile("test_files/1_iphone5.txt"),
+	                                readFile("test_files/2_iphone5.txt"),
+	                                readFile("test_files/3_iphone5.txt"),
+	                                readFile("test_files/4_iphone5.txt"),
+	                                readFile("test_files/5_iphone5.txt"),
+	                                readFile("test_files/6_iphone5.txt"),
+	                                readFile("test_files/1_nasa.txt"),
+	                                readFile("test_files/2_nasa.txt"),
+	                                readFile("test_files/3_nasa.txt"),
+	                                readFile("test_files/4_nasa.txt"),
+	                                readFile("test_files/5_nasa.txt"),
+	                                readFile("test_files/1_xijinping.txt"),
+	                                readFile("test_files/2_xijinping.txt"),
+	                };
+	                String[] ids = {"romney_1", "romney_2", "romney_3", "romney_4", "romney_5", "romney_6",
+	                                "iphone_1", "iphone_2", "iphone_3", "iphone_4", "iphone_5", "iphone_6",
+	                                "nasa_1", "nasa_2", "nasa_3", "nasa_4", "nasa_5",
+	                                "xijinping_1", "xijinping_2"
+	                };              
+
+	                TFCalculator tfc = new TFCalculator(input, ids);
+	                System.out.println("vector size = " + tfc.tfWords.size());
+	                for (String s : tfc.tfWords) {
+	                        System.out.println(s);                  
+	                }
+	                System.out.println();
+
+	              try {
+	              //[SINGLE|COMPLETE|AVERAGE|MEAN|CENTROID|WARD|ADJCOMLPETE|NEIGHBOR_JOINING]
+	              HClusterer hc = new HClusterer("AVERAGE", 2, 6, 10);
+	              String jsonHierachy = hc.getJsonHierachy(new ByteArrayInputStream(tfc.getResult().getBytes("UTF-8")));
+	              System.out.println(jsonHierachy);
+	              String result = hc.getClusters(jsonHierachy);
+	              System.out.println(result);
+	              JSONArray jsonResult = new JSONArray(result);
+	              System.out.println(jsonResult.length() + " clusters:");
+	              for (int i = 0; i < jsonResult.length(); ++i)
+	                  System.out.println(jsonResult.get(i));
+	          } catch (Exception e) {
+	              e.printStackTrace();
+	          }
+	                
+	        }
 }
